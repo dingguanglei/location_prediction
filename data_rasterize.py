eece472fcb3ebbde27723b44789819daa2b71d75
ElaineBai <45814760+ElaineBai@users.noreply.github.com>
2018-12-12 21:03:47 +0800

    Update data_rasterize.py

--- data_rasterize.py
+++ data_rasterize.py
@@ -4,8 +4,9 @@ import matplotlib.cm as cm
 import xlrd
 from matplotlib.colors import LogNorm
 import numpy as np
+import os
 
-def rasterize(path):
+def rasterize(num_excel, path, save_fig):
 
     data = xlrd.open_workbook(path)
     table = data.sheet_by_name('Sheet1')
@@ -28,12 +29,11 @@ def rasterize(path):
         result = []
         arr = arr.tolist()
         for i in arr:
-            if -15<=i[0]<15 and -15 <= i[1] <15:
+            if -15 <= i[0] < 15 and -15 <= i[1] < 15:
                 x = arr.count(i)
                 result.append(i+[x])
         res = np.array(sorted(list(set([tuple(t) for t in result]))))
         res = res.astype(int)
-        # print(res)
 
         arr = np.zeros((30, 30))
         x, y, z = res[:, 0], res[:, 1], res[:, 2]
@@ -41,8 +41,16 @@ def rasterize(path):
             arr[y[i]+15, x[i]+15] = z[i]
         seq = np.concatenate(arr, axis=0)
         seq_arr.append(seq)
-        # plt.imshow(arr, extent=(np.amin(x), np.amax(x), np.amin(y), np.amax(y)), cmap=cm.rainbow, norm=LogNorm())
-        # plt.colorbar()
-        # plt.show()
+        if save_fig:
+            plt.imshow(arr, extent=(-750, 750, -750, 750), cmap=cm.rainbow, norm=LogNorm(), origin='lower')
+            plt.colorbar()
+            # plt.show()
+            if not os.path.exists("trueDataMap"):
+                os.mkdir("trueDataMap")
+            if not os.path.exists(os.path.join("trueDataMap", "excel{}".format(num_excel))):
+                os.mkdir(os.path.join("trueDataMap", "excel{}".format(num_excel)))
+            plt.savefig("trueDataMap/excel{}/time{}.jpg".format(num_excel, n+1))
+            plt.close()
+            print("Raw figure saved to trueDataMap/excel{}/time{}.jpg".format(num_excel, n+1))
 
     return seq_arr
